7aebe138841332c3e496dcf41bd88f8593f2f0fe
SHUBHAM BATWAL <156819851+shubhambatwal03@users.noreply.github.com>
2026-07-21 20:58:14 +0530

    Updated Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
   const [showAdmin, setShowAdmin] = useState(false);
 
   return (
-    <nav className="sticky border-b border-b-gray-500 bg-slate-900 text-white mb-3">
+    <nav className="sticky border-b-gray-500 bg-slate-900 text-white">
       <div className="w-full flex justify-baseline px-6 py-8">
         <a href="/" className="flex items-center gap-3">
           <div className="flex h-12 w-12 items-center justify-center rounded-xl bg-sky-600 text-white shadow-lg">
@@ -20,24 +20,7 @@ function Navbar() {
           </div>
         </a>
 
-        {/* Desktop Links */}
-        <div className="hidden items-center gap-3 md:flex ml-auto">
-          <a
-            href="tel:7720995877"
-            className="flex items-center gap-2 rounded-xl border border-sky-600 px-4 py-2 font-medium text-sky-600 transition hover:bg-sky-600 hover:text-white"
-          >
-            <Phone size={18} />
-            Call
-          </a>
-
-          <a
-            href="#booking"
-            className="flex items-center gap-2 rounded-xl bg-sky-600 px-5 py-2 font-semibold text-white shadow-md transition hover:bg-sky-700"
-          >
-            <Ticket size={18} />
-            Book Now
-          </a>
-
+        <div className="items-center gap-3 md:flex ml-auto">
           <button
             onClick={() => setShowAdmin(true)}
             className="flex items-center gap-2 rounded-xl bg-slate-900 px-5 py-2 font-semibold text-white border transition hover:bg-slate-800"
@@ -46,46 +29,7 @@ function Navbar() {
             Admin
           </button>
         </div>
-
-        {/* Mobile Humber */}
-        <button
-          onClick={() => setIsOpen(!isOpen)}
-          className="rounded-lg p-2 text-slate-700 hover:bg-slate-100 md:hidden ml-auto"
-        >
-          {isOpen ? <X size={28} /> : <Menu size={28} />}
-        </button>
       </div>
-
-      {/* Mobile Links */}
-      {isOpen && (
-        <div className="border-t bg-white md:hidden">
-          <div className="space-y-2 px-6 py-5">
-            <a
-              href="tel:7720995877"
-              className="flex items-center gap-2 rounded-lg bg-green-600 px-4 py-3 font-semibold text-white"
-            >
-              <Phone size={18} />
-              Call Now
-            </a>
-
-            <a
-              href="#booking"
-              className="flex items-center gap-2 rounded-lg bg-sky-600 px-4 py-3 font-semibold text-white"
-            >
-              <Ticket size={18} />
-              Book Seat
-            </a>
-
-            <button
-              onClick={() => setShowAdmin(true)}
-              className="flex w-full items-center gap-2 rounded-lg bg-slate-900 px-4 py-3 font-semibold text-white"
-            >
-              <LayoutDashboard size={18} />
-              Admin Dashboard
-            </button>
-          </div>
-        </div>
-      )}
       {showAdmin && <Admin onClose={() => setShowAdmin(false)} />}
     </nav>
   );
